f4e8d38793cc553f4f2e5124122780991df2b8be
Sam Hagin <samhagin@yahoo.com>
2020-02-28 13:29:02 -0600

    Update instagram_download.py

--- instagram_download.py
+++ instagram_download.py
@@ -33,10 +33,10 @@ for post in posts:
   type = driver.find_element_by_xpath('//meta[@property="og:type"]').get_attribute('content')
   if type == 'video':
       download_url = driver.find_element_by_xpath("//meta[@property='og:video']").get_attribute('content')
-      urllib.request.urlretrieve( download_url , '/Users/code/Instagram/{}.mp4'.format( shortcode ))
+      urllib.request.urlretrieve( download_url , '{}.mp4'.format( shortcode ))
   else:
       download_url = driver.find_element_by_xpath("//meta[@property='og:image']").get_attribute('content')
-      urllib.request.urlretrieve(download_url, '/Users/code/Instagram/{}.jpg'.format(shortcode))
+      urllib.request.urlretrieve(download_url, '{}.jpg'.format(shortcode))
   time.sleep( 5 )
 
 print("Done")
